97d520274a11446ebfb436d1959b218bfe892143
Mark Roszko <mark.roszko@gmail.com>
2015-03-25 20:40:26 -0400

    Quick template out of home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,26 @@
 @extends('app')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
 
-				<div class="panel-body">
-					You are logged in!
-				</div>
-			</div>
-		</div>
+
+<style type='text/css'>
+.jumbotron {
+  text-align: center;
+  background-color: transparent;
+}
+</style>
+<div class="container">
+	<div class="jumbotron">
+		<h1>Find your KiCad libraries, components and footprints!</h1>
+	</div>
+	<div class="row">
+		
+		<div class="input-group">
+			<input type="text" class="form-control" placeholder="Search libraries....">
+			<span class="input-group-btn">
+				<button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+			</span>
+		</div><!-- /input-group -->
 	</div>
 </div>
 @endsection
